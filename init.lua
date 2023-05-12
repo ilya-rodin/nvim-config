a1e6adc94fda43c1305d660fa512fa8bf40bf121
Ilya Rodin <i1.rodin@yandex.ru>
2023-05-13 01:15:51 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
     },
 
     -- Set colorscheme to use
-    colorscheme = "tokyonight",
+    colorscheme = "nightfox",
     background = "light",
 
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
